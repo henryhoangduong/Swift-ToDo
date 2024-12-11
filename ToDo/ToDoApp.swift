0e28b248bc3509a3c83d586a5107f7508ee9ae15
henryhoangduong <henryhoangduong@gmail.com>
2024-12-11 23:53:15 +0700

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by mac on 11/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoTests/ToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by mac on 11/12/2567 BE.
+//
+
+import Testing
+@testable import ToDo
+
+struct ToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
