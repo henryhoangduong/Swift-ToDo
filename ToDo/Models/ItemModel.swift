62092d6f3554636cf22027d99af248abaaf0dad7
henryhoangduong <henryhoangduong@gmail.com>
2024-12-12 15:53:51 +0700

    add model

---
+++ ToDo/Models/ItemModel.swift
@@ -0,0 +1,14 @@
+//
+//  ItemModel.swift
+//  ToDo
+//
+//  Created by mac on 12/12/2567 BE.
+//
+
+import Foundation
+
+struct ItemModel:Identifiable {
+    let id:String = UUID().uuidString
+    let title: String
+    let isComplete:Bool
+}
--- ToDo/Views/ListView.swift
+++ ToDo/Views/ListView.swift
@@ -8,16 +8,24 @@
 import SwiftUI
 
 struct ListView: View {
-    @State var item: [String] = ["this is the first title", "this is the second title", "this the third title"]
+    @State var items: [ItemModel] = [
+        ItemModel(title: "This is the first title", isComplete: true),
+        ItemModel(title: "This is the second title", isComplete: true),
+        ItemModel(title: "This is the third title", isComplete: true)
+
+    ]
     var body: some View {
         NavigationView {
             List {
-                ForEach(item, id: \.self) {
-                    item in ListRowView(title: item)
+                ForEach(items) {
+                    item in Text("Hi")
+                    // ListRowView()
                 }
-            }.listStyle(PlainListStyle())
-                .navigationTitle("To do list 📝").navigationBarItems(leading: EditButton(),
-                                                                    trailing: NavigationLink("Add", destination: AddView()))
+            }
+            .listStyle(PlainListStyle())
+            .navigationTitle("To do list 📝")
+            .navigationBarItems(leading: EditButton(),
+            trailing: NavigationLink("Add", destination: AddView()))
         }
     }
 }
